52162dfea5ebece62b3f58ce640663d70ff34c0c
sharpiro <dsharpbb09@gmail.com>
2025-01-22 12:50:21 -0500

    cpp building wgpu win32

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,13 +1,60 @@
-#include <cstddef>
-#include <cstdint>
 #include <cstdio>
 #include <format>
+#include <magic_enum.hpp>
 #include <print>
+#include <vector>
 #include <webgpu/webgpu.h>
 
-int main() {
-  WGPUInstanceDescriptor desc = {};
-  WGPUInstance instance = wgpuCreateInstance(&desc);
+using namespace std;
 
-  // std::println("desc {}", (void *)desc.nextInChain);
+WGPUAdapter get_adapter(WGPUInstance instance) {
+    WGPUAdapter adapter = nullptr;
+    auto adapter_callback = [](WGPURequestAdapterStatus,
+                               WGPUAdapter adapter,
+                               char const *,
+                               WGPU_NULLABLE void *userdata) {
+        WGPUAdapter *const local_adapter = static_cast<WGPUAdapter *>(userdata);
+        *local_adapter = adapter;
+    };
+    wgpuInstanceRequestAdapter(instance, nullptr, adapter_callback, &adapter);
+    return adapter;
+}
+
+int main() {
+    /* Init */
+    println("starting");
+
+    WGPUInstanceDescriptor desc = {};
+    WGPUInstance instance = nullptr;
+    instance = wgpuCreateInstance(&desc);
+    if (!instance) {
+        println(stderr, "expected instance");
+        return 1;
+    }
+
+    auto adapter = get_adapter(instance);
+    if (!adapter) {
+        println(stderr, "expected adapter");
+        return 1;
+    }
+    wgpuInstanceRelease(instance);
+
+    size_t feature_count = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+    println("features: {}", feature_count);
+    auto features = vector<WGPUFeatureName>(feature_count);
+    wgpuAdapterEnumerateFeatures(adapter, features.data());
+
+    WGPUAdapterInfo adapter_info;
+    wgpuAdapterGetInfo(adapter, &adapter_info);
+    println(
+        "device: {}\nbackend: {}\nadapter: {}",
+        adapter_info.device,
+        magic_enum::enum_name(adapter_info.backendType),
+        magic_enum::enum_name(adapter_info.adapterType)
+    );
+
+    /* Cleanup */
+
+    wgpuAdapterRelease(adapter);
+    println("done");
 }
